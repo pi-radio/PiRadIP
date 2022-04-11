3376b0a22400991890f54262feecac0a3f8854d2
Michael Zappe <zapman@z-meta.com>
2022-04-11 12:27:50 -0600

    Now building library component.xml

--- build/__init__.py
+++ build/__init__.py
@@ -1,9 +1,12 @@
 from .structure import add_library_file, add_interface, add_module
 from .interfaces import build_interfaces
 from .modules import build_modules, wrap_modules
+from .ipxact import build_libraries
 
 def build_all():
     print("Building all...")
+    build_libraries()
+    
     build_interfaces()
 
     build_modules()
--- build/ipxact.py
+++ build/ipxact.py
@@ -1,7 +1,15 @@
 from .piradip_build_base import *
 
+from .structure import piradlib_files
+
 from .ipxact_base import *
 from .ipxact_bus import *
+
+import lxml.etree
+import io
+import datetime
+import os
+import os.path
 
 class IPXACTChoiceList:
     def __init__(self, name, values):
@@ -19,6 +27,120 @@ interrupt_sensitivity_list = IPXACTChoiceList("choice_list_99a1d2b8", [ 'LEVEL_H
 polarity_list = IPXACTChoiceList("choice_list_9d8b0d81", [ 'ACTIVE_HIGH', 'ACTIVE_LOW' ] )
 
 direction_map = { 'input': 'in', 'output': 'out' }
+
+class IPXACTLibrary():
+    def __init__(self, library_files):
+        library_name = "PiRadIP"
+        library_version = "1.0"
+        library_description = "Pi Radio IP Library"
+
+        self.library_path = os.path.join(os.getcwd(), "library")
+
+        print("Library path: ", self.library_path)
+        
+        synthesis_fileset_name = "xilinx_anylanguagesynthesis_view_fileset"
+        simulation_fileset_name = "xilinx_anylanguagebehavioralsimulation_view_fileset"
+        
+        self.synthesis_view = S.view(
+            S.name("xilinx_anylanguagesynthesis"),
+            S.displayName("Synthesis"),
+            S.envIdentifier(":vivado.xilinx.com:synthesis"),
+            S.language("SystemVerilog"),
+            S.fileSetRef(synthesis_fileset_name)
+        )
+
+        self.simulation_view = S.view(
+            S.name("xilinx_anylanguagebehavioralsimulation"),
+            S.displayName("Simulation"),
+            S.envIdentifier(":vivado.xilinx.com:simulation"),
+            S.language("SystemVerilog"),
+            S.fileSetRef(simulation_fileset_name)
+        )
+        
+        self.model = S.model(self.synthesis_view,
+                             self.simulation_view)
+
+        self.synthesis_fileset = S.fileSet(
+            S.name(synthesis_fileset_name)
+            )
+
+        self.simulation_fileset = S.fileSet(
+            S.name(simulation_fileset_name)
+            )        
+        
+        self.file_sets = S.fileSets(self.synthesis_fileset,
+                                    self.simulation_fileset)
+
+        for filename in library_files:
+            relative_filename = os.path.relpath(filename, self.library_path)
+            
+            self.synthesis_fileset.append(
+                S.file(
+                    S.name(relative_filename),
+                    S.fileType("systemVerilogSource"),
+                    S.logicalName("library")
+                )
+            )
+            
+            self.simulation_fileset.append(
+                S.file(
+                    S.name(relative_filename),
+                    S.fileType("systemVerilogSource"),
+                    S.logicalName("library")
+                )
+            )
+
+        
+        self.parameters = S.parameters()
+
+        self.xilinx_core_ext = X.coreExtensions(
+            X.taxonomies(
+                X.taxonomy("/BaseIP"),
+                X.taxonomy("/Embedded_Processing/AXI_Infrastructure"),
+                X.taxonomy("/Memories_&_Storage_Elements"),
+            ),
+            X.displayName("PiRadIP_v1_0"),
+            X.hideInCatalogGUI("true"),
+            X.definitionSource("package_project"),
+            X.xpmLibraries(
+                X.xpmLibrary("XPM_CDC"),
+                X.xpmLibrary("XPM_FIFO"),
+                X.xmpLibrary("XPM_MEMORY")
+                ),
+            X.vendorDisplayName("Pi Radio Inc."),
+            X.vendorURL("https://pi-rad.io/"),
+            X.coreRevision("1"),
+            X.upgrades(X.canUpgradeFrom("pi-rad.io:ip:library:1.0")),
+            X.coreCreationDateTime(datetime.datetime.utcnow().strftime("%Y-%M-%jT%H:%M:%SZ"))
+            )
+        
+        self.component = S.component(
+            S.vendor("pi-rad.io"),
+            S.library("piradip"),
+            S.name(library_name),
+            S.version(library_version),
+            self.model,
+            self.file_sets,
+            S.description(library_description),
+            self.parameters,
+            S.vendorExtensions(self.xilinx_core_ext)
+            )
+
+    def xml_path(self):
+        return os.path.join(self.library_path, "component.xml")
+        
+    def export_ipxact(self, f):
+        print(lxml.etree.tostring(self.component, encoding='UTF-8', pretty_print=True, xml_declaration=True).decode(), file=f)
+
+        
+def build_libraries():
+    l = IPXACTLibrary(piradlib_files)
+
+    f = open(l.xml_path(), "w")
+    
+    l.export_ipxact(f)
+    
+        
 
 class IPXACTModule():
     def __init__(self, module):
@@ -111,8 +233,7 @@ class IPXACTModule():
         
 
     def export_ipxact(self, f):
-        print(etree.tostring(self.component, pretty_print=True).decode(), file=f)
-
+        print(lxml.etree.tostring(self.component,  encoding='UTF-8', pretty_print=True, xml_declaration=True).decode(), file=f)
 
     def add_port(self, name, direction, typename):
         
